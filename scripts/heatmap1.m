e1ad4ed9c1836074de03978df4cb66d40d0e9e1f
Chittaranjan Swaminathan <ksatyaki@gmail.com>
2018-03-08 14:53:01 +0100

    delete experiment files from master

--- scripts/heatmap1.m
+++
@@ -1,8 +0,0 @@
-function map = heatmap1(path, resolution, maps)
-%map = uint8(zeros(size(maps)) * 255);
-for i=1:size(path,1)
-  maps(round(path(i,2)/resolution), round(path(i,1)/resolution)) = ...
-  maps(round(path(i,2)/resolution), round(path(i,1)/resolution)) + 1;
-end
-map = maps;
-end
